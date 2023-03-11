4325cd3a31c34bd01aa5382f82b72df304f557cb
gusalbukrk <gusalbukrk@gmail.com>
2023-03-11 00:06:41 -0300

    fix(`api`): improve handling of breakdown options

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -9,26 +9,77 @@ import {
 
 import 'cross-fetch/dist/node-polyfill';
 
-const app = express();
-
-type baseType = {
-  query: string;
+type optionsType = Partial<{
   unit: unitType;
   quantity: number;
   format: formatType;
-};
 
-type inputType = {
-  sentencesPerParagraphMin: number;
-  sentencesPerParagraphMax: number;
-  wordsPerSentenceMin: number;
-  wordsPerSentenceMax: number;
-} & baseType;
-
-type optionsType = {
+  // breakdown options
   sentencesPerParagraph: Partial<sentencesPerParagraphType>;
   wordsPerSentence: Partial<wordsPerSentenceType>;
-} & baseType;
+}>;
+
+type inputsType = { query: string } & optionsType;
+
+// requests with query parameters or route parameters doesn't support objects
+// they may contain properties like wordsPerSentenceMin that must be converted to
+// wordsPerSentence.min before being passed to fullfiller function
+type flatInputsType = Omit<
+  inputsType,
+  'sentencesPerParagraph' | 'wordsPerSentence'
+> &
+  Partial<{
+    sentencesPerParagraphMin: number;
+    sentencesPerParagraphMax: number;
+    wordsPerSentenceMin: number;
+    wordsPerSentenceMax: number;
+  }>;
+
+const parseIntRadix10 = (n: string) => parseInt(n, 10);
+
+// handles breakdown options (i.e. sentencesPerParagraph and wordsPerSentence)
+// for instance, convert wordsPerSentenceMin to wordsPerSentence.min
+function unflattenBreakdownOptions(
+  inputs: flatInputsType
+): Partial<inputsType> {
+  return {
+    sentencesPerParagraph: {
+      ...(inputs.sentencesPerParagraphMin !== undefined
+        ? {
+            min: parseIntRadix10(
+              inputs.sentencesPerParagraphMin as unknown as string
+            ),
+          }
+        : {}),
+      ...(inputs.sentencesPerParagraphMax !== undefined
+        ? {
+            max: parseIntRadix10(
+              inputs.sentencesPerParagraphMax as unknown as string
+            ),
+          }
+        : {}),
+    },
+
+    wordsPerSentence: {
+      ...(inputs.wordsPerSentenceMin !== undefined
+        ? {
+            min: parseIntRadix10(
+              inputs.wordsPerSentenceMin as unknown as string
+            ),
+          }
+        : {}),
+      ...(inputs.wordsPerSentenceMax !== undefined
+        ? {
+            max: parseIntRadix10(
+              inputs.wordsPerSentenceMax as unknown as string
+            ),
+          }
+        : {}),
+    },
+  };
+}
+
+const app = express();
 
 // middleware
 app.use(express.json()); // parse application/json
@@ -40,67 +91,60 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
+// endpoint handles requests of 2 types:
+// - requests with query parameters, e.g. `?query=harry potter&format=html`
+// - requests with a body containing json or urlencoded data
 app.get('/api/', async (req, res) => {
-  // endpoint will receive requests of 2 types:
-  // - requests with query parameters, e.g. `?query=harry potter&format=html`
-  // - requests with bodies containing json or urlencoded data
-
-  const { query, ...options } =
+  const { query, ...options } = (
     Object.keys(req.query).length !== 0
-      ? (req.query as unknown as optionsType)
-      : (req.body as unknown as optionsType);
+      ? {
+          // if request is using query parameters, need to convert breakdown options to objects
+          // example: wordsPerSentenceMin => wordsPerSentence.min
+          ...Object.fromEntries(
+            // filter out breakdown options (e.g. wordsPerSentenceMin)
+            Object.entries(req.query).filter(
+              (
+                [k, v] // eslint-disable-line @typescript-eslint/no-unused-vars
+              ) =>
+                ![
+                  'sentencesPerParagraphMin',
+                  'sentencesPerParagraphMax',
+                  'wordsPerSentenceMin',
+                  'wordsPerSentenceMax',
+                ].includes(k)
+            )
+          ),
+          ...unflattenBreakdownOptions(req.query as unknown as flatInputsType),
+        }
+      : // if request is using body, breakdown options already are objects
+        req.body
+  ) as inputsType;
 
-  if (options.quantity)
-    options.quantity = parseInt(options.quantity as unknown as string, 10);
+  if ('quantity' in options)
+    options.quantity = parseIntRadix10(options.quantity as unknown as string);
 
   const article = await fullfiller(query, options);
 
   res.status(200).json(article);
 });
 
+// endpoint handles requests with route parameters (also known as path)
 app.get(
   '/api/:query/:unit?/:quantity?/:format?/:sentencesPerParagraphMin?/:sentencesPerParagraphMax?/:wordsPerSentenceMin?/:wordsPerSentenceMax?',
-  async (req: { params: inputType }, res) => {
+  async (req: { params: flatInputsType }, res) => {
     const { params } = req;
-
     const { query } = params;
 
-    const options: Partial<optionsType> = {
+    const options: Omit<inputsType, 'query'> = {
       ...(params.unit !== undefined ? { unit: params.unit } : {}),
       ...(params.quantity !== undefined
-        ? { quantity: parseInt(params.quantity as unknown as string, 10) }
+        ? { quantity: parseIntRadix10(params.quantity as unknown as string) }
         : {}),
       ...(params.format !== undefined ? { format: params.format } : {}),
-
-      sentencesPerParagraph: {},
-      wordsPerSentence: {},
+      ...unflattenBreakdownOptions(params),
     };
 
-    if (params.sentencesPerParagraphMin !== undefined)
-      options.sentencesPerParagraph!.min = parseInt(
-        params.sentencesPerParagraphMin as unknown as string,
-        10
-      );
-
-    if (params.sentencesPerParagraphMax !== undefined)
-      options.sentencesPerParagraph!.max = parseInt(
-        params.sentencesPerParagraphMax as unknown as string,
-        10
-      );
-
-    if (params.wordsPerSentenceMin !== undefined)
-      options.wordsPerSentence!.min = parseInt(
-        params.wordsPerSentenceMin as unknown as string,
-        10
-      );
-
-    if (params.wordsPerSentenceMax !== undefined)
-      options.wordsPerSentence!.max = parseInt(
-        params.wordsPerSentenceMax as unknown as string,
-        10
-      );
-
-    const article = await fullfiller(query, options); // eslint-disable-line @typescript-eslint/no-non-null-assertion
+    const article = await fullfiller(query, options);
 
     res.status(200).json(article);
   }
