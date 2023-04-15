f655d2f113bf17b6af00ab5a956571e0dda4cd4b
gusalbukrk <gusalbukrk@gmail.com>
2023-04-15 00:21:47 -0300

    feat(cli): add options to interactive mode

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,18 @@
 import { program } from 'commander';
 import fullfiller from 'fullfiller/src';
-import { unflattenBreakdownOptions } from 'fullfiller-common/src/utils';
+import {
+  sentencesPerParagraphDefault,
+  wordsPerSentenceDefault,
+} from 'fullfiller-common/src/constants';
+import {
+  unitType,
+  flatOptionsType,
+  optionsType,
+} from 'fullfiller-common/src/types';
+import {
+  unflattenBreakdownOptions,
+  parseIntR10,
+} from 'fullfiller-common/src/utils';
 import inquirer from 'inquirer';
 
 import pkg from '../package.json';
@@ -12,61 +24,98 @@ import 'cross-fetch/dist/node-polyfill';
     .name('fullfiller')
     .description('feature-rich filler text generator')
     .version(pkg.version, '-v, --version')
-    .argument('[query]', 'Wikipedia query string') // optional only for interactive mode
+    // optional only for interactive mode; if not provided in normal mode,
+    // it's passed to fullfiller as undefined triggering invalidInput error
+    .argument('[query]', 'Wikipedia query string')
     .option('-i, --interactive', 'interactive mode')
     .option('-u, --unit <string>', '`paragraphs` or `words`')
     .option(
       '-q, --quantity <number>',
       'integer, defaults to 5 for `paragraphs` and 200 for `words`',
-      parseInt
+      parseIntR10
     )
     .option('-f, --format <string>', '`plain` (default) or `html`')
-    .option(
-      '--no-stringify', // a negatable boolean (leading 'no-') is true by default
-      'return an array instead of string'
-    )
+    // a negatable boolean (leading 'no-') without corresponding regular boolean is true by default
+    .option('--no-stringify', 'return an array instead of string')
     .option(
       '--sentencesPerParagraphMin <number>',
       'min quantity of sentences per paragraph',
-      parseInt
+      parseIntR10
     )
     .option(
       '--sentencesPerParagraphMax <number>',
       'max quantity of sentences per paragraph',
-      parseInt
+      parseIntR10
     )
     .option(
       '--wordsPerSentenceMin <number>',
       'min quantity of words per sentence',
-      parseInt
+      parseIntR10
     )
     .option(
       '--wordsPerSentenceMax <number>',
       'max quantity of words per sentence',
-      parseInt
+      parseIntR10
     );
 
   program.parse();
 
-  if (program.opts().interactive === true) {
-    console.log('interactive mode');
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const answers = await inquirer.prompt([
+  const answers = (program.opts().interactive /* true or undefined */ &&
+    (await inquirer.prompt([
       {
-        type: 'input',
         name: 'query',
+        validate: (input: string) =>
+          input.length !== 0 ? true : 'Please enter a non-empty string.',
       },
-    ]);
+      {
+        name: 'unit',
+        type: 'list',
+        choices: ['paragraphs', 'words'],
+      },
+      {
+        name: 'quantity',
+        type: 'number',
+        default: ({ unit }: { unit: unitType }) =>
+          unit === 'paragraphs' ? 5 : 200,
+      },
+      {
+        name: 'format',
+        type: 'list',
+        choices: ['plain', 'html'],
+      },
+      {
+        name: 'stringify',
+        type: 'confirm',
+      },
+      {
+        name: 'sentencesPerParagraphMin',
+        type: 'number',
+        default: sentencesPerParagraphDefault.min,
+      },
+      {
+        name: 'sentencesPerParagraphMax',
+        type: 'number',
+        default: sentencesPerParagraphDefault.max,
+      },
+      {
+        name: 'wordsPerSentenceMin',
+        type: 'number',
+        default: wordsPerSentenceDefault.min,
+      },
+      {
+        name: 'wordsPerSentenceMax',
+        type: 'number',
+        default: wordsPerSentenceDefault.max,
+      },
+    ]))) as flatOptionsType & { query: string };
 
-    console.log(answers);
-  } else {
-    const query = program.args[0];
-    const options = unflattenBreakdownOptions(program.opts());
+  const { query, ...options } = unflattenBreakdownOptions(
+    answers ?? { query: program.args[0], ...program.opts<flatOptionsType>() }
+  ) as optionsType & { query: string };
 
-    const filler = await fullfiller(query, options);
+  console.log('\nloading...\n');
+  const filler = await fullfiller(query, options);
 
-    // using console.dir instead of console.log because log only show the first 2 levels of depth
-    console.dir(filler, { depth: null, colors: true }); // eslint-disable-line no-console
-  }
+  // using console.dir instead of console.log because log only show the first 2 levels of depth
+  console.dir(filler, { depth: null, colors: true }); // eslint-disable-line no-console
 })().catch((e) => console.error(e)); // eslint-disable-line no-console
