6f9afb149202bf6cb41bce0290530a2d9a6d0fbb
gusalbukrk <gusalbukrk@gmail.com>
2024-01-24 10:05:10 -0300

    config(site): edit `package.json`, add stylelint to vite

---
+++ packages/vite-project/.stylelintrc.js
@@ -0,0 +1,18 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
+  plugins: ['stylelint-order', 'stylelint-prettier'],
+  rules: {
+    'prettier/prettier': true,
+    'order/properties-alphabetical-order': true,
+  },
+  overrides: [
+    {
+      files: ['**/*.{sass,scss}'],
+      rules: {
+        // rule is only appropriate for CSS, you should not turn it on for Sass
+        'media-query-no-invalid': null,
+      },
+    },
+  ],
+};
